1088c7eadf4e485ca0c523a9cd3745743eaf2391
JasonChoi <ccc11203@gmail.com>
2018-12-11 11:52:59 +0900

    prac3 - initial project files

---
+++ prac3/src/main/java/me/jasonchoi/prac3/Prac3Application.java
@@ -0,0 +1,12 @@
+package me.jasonchoi.prac3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prac3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prac3Application.class, args);
+    }
+}
