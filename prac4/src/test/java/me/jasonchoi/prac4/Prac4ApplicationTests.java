11c16d9951df484931e1342dfc5256ea06908cde
JasonChoi <ccc11203@gmail.com>
2018-12-14 10:24:46 +0900

    application.properties, yml

---
+++ prac4/src/test/java/me/jasonchoi/prac4/Prac4ApplicationTests.java
@@ -0,0 +1,17 @@
+package me.jasonchoi.prac4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Prac4ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
