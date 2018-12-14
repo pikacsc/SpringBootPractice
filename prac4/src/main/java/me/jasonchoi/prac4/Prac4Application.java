395db3f6847d560a879007aceadc286fda6d70ed
JasonChoi <ccc11203@gmail.com>
2018-12-14 10:20:59 +0900

    prac4 - build project, pom.xml

---
+++ prac4/src/main/java/me/jasonchoi/prac4/Prac4Application.java
@@ -0,0 +1,14 @@
+package me.jasonchoi.prac4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prac4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prac4Application.class, args);
+    }
+
+}
+
